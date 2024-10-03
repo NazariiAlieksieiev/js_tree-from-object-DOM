134e194621a7674bcbe7bd34aca736e6a058215a
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2024-10-03 22:15:49 +0300

    make build

---
+++ index.5e0303c3.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in n.append(t),r){var c=document.createElement("li");c.innerText=a,t.append(c),Object.keys(r[a]).length>0&&e(c,r[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.5e0303c3.js.map
